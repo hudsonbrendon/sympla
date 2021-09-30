97442c4045fc3040295b34f12f567443c2a92c9c
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2021-09-29 21:13:21 -0300

    Added type hint in methods

--- examples/participants_by_order.py
+++ examples/participants_by_order.py
@@ -4,4 +4,8 @@ from decouple import config
 
 sympla = Sympla(token=config("TOKEN"))
 
-print(sympla.participants_by_order(event_id=config("EVENT_ID"), order_id=config("ORDER_ID")))
+print(
+    sympla.participants_by_order(
+        event_id=config("EVENT_ID"), order_id=config("ORDER_ID")
+    )
+)
